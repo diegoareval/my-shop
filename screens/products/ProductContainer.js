adfdbd47f517a970cc7179ef2862b8b71da20e77
Diego <diego2000avelar@gmail.com>
2021-01-24 09:16:36 -0600

    file structure

---
+++ screens/products/ProductContainer.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react'
+import {ActivityIndicator, StyleSheet, View, Text} from "react-native"
+
+const ProductContainer = () => {
+    return (
+        <View>
+            <Text>Producto Container</Text>
+        </View>
+    )
+}
+
+export default ProductContainer
